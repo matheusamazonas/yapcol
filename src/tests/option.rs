ef19452f1bcee68e4815637109cd1f05ac3f3f8b
Matheus Amazonas <matheus.amazonas@gmail.com>
2026-03-17 16:55:00 +0100

    ✅ Improve `option`'s failing test cases
    Checking if the end of input isn't enough. Checking for the next token is safer.

--- src/tests/option.rs
+++ src/tests/option.rs
@@ -32,14 +32,14 @@ fn fail_not_consuming() {
 	let mut input = Input::new(tokens);
 	let parse_option = option(&parser1, &parser2);
 	assert_eq!(parse_option(&mut input), Err(Error::UnexpectedToken));
-	assert!(end_of_input()(&mut input).is_err()); // Ensure that the input was NOT consumed.
+	assert_eq!(any()(&mut input), Ok("other")); // Ensure that the input was NOT consumed.
 }
 
 #[test]
 fn fail_consuming() {
 	let parser1 = is("hello");
 	let parser2 = is("hallo");
-	let tokens = vec!["hello", "hello"];
+	let tokens = vec!["hello", "hollo"];
 	let mut input = Input::new(tokens);
 	let consuming_parser = |input: &mut Input<_>| {
 		parser1(input)?;
@@ -48,4 +48,5 @@ fn fail_consuming() {
 	let parse_option = option(&consuming_parser, &parser2);
 	let output = parse_option(&mut input);
 	assert_eq!(output, Err(Error::UnexpectedToken));
+	assert_eq!(any()(&mut input), Ok("hollo")); // Ensure that the input was consumed.
 }
